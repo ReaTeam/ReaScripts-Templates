e4d2da6b10cbc3654a22150ba60f36e1e0935658
X-Raym <extremraym@yahoo.fr>
2016-02-13 19:41:12 +0100

    update

--- Tracks/X-Raym_Get track depth level.lua
+++ Tracks/X-Raym_Get track depth level.lua
@@ -1,14 +1,7 @@
--- Display a message in the console for debugging
-function Msg(value)
-  if console then
-    reaper.ShowConsoleMsg(tostring(value) .. "\n")
-  end
-end
-
 function GetTrackFolderLevel(track)
   local idx = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
   local level = 0
-  if idx > 1 then
+  if idx > 1 then -- if not the first track (first track will always be 0)
     for z = 1, idx -1 do
       local track = reaper.GetTrack(0, z-1)
       local depth = reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
@@ -18,7 +11,15 @@ function GetTrackFolderLevel(track)
   return level
 end
 
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
 
+----------------------------------------
+-- RENAME (for debugging)
 function RenameTracksWithFolderDepth()
   for i = 0, reaper.CountTracks(0) - 1 do
     local track = reaper.GetTrack(0,i)

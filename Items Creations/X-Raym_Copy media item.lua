6da724b54559894707f043a6cb5d507e5aee11e5
X-Raym <extremraym@yahoo.fr>
2016-02-29 20:56:42 +0100

    update

--- Items Creations/X-Raym_Copy media item.lua
+++ Items Creations/X-Raym_Copy media item.lua
@@ -1,25 +1,42 @@
--- Copy Media Item to Position and Track
+-- Create a Text Item.
 -- Return the item if success. Else, return nil.
 function CopyMediaItem(item, track, position)
     
 	local new_item = reaper.AddMediaItemToTrack(track)
-	
+	local new_item_guid = reaper.BR_GetMediaItemGUID(new_item)
+	Msg(new_item_guid)
 	local retval, item_chunk =  reaper.GetItemStateChunk(item, '')
+	new_item_chunk = item_chunk:gsub('IGUID ({.+})', 'IGUID ' .. new_item_guid )
 	
-	reaper.SetItemStateChunk(new_item, item_chunk)
-  
+	Msg(item_chunk)
+	Msg(new_item_chunk)
+		
+	reaper.SetItemStateChunk(new_item, new_item_chunk)
+	
 	reaper.SetMediaItemInfo_Value(new_item, "D_POSITION", position)
 	
 	reaper.UpdateItemInProject(new_item)
-
+	
 	return new_item
 
 end
 
--- Copy first selected item on first track at position 10
+
+console = true
+function Msg(value)
+	if console then
+		reaper.ShowConsoleMsg(tostring(value) .. "\n")
+	end
+end
+
+
+-- Copy first selected item on first track
 sel_item = reaper.GetSelectedMediaItem(0,0)
 if sel_item then
 	track = reaper.GetTrack(0, 0)
 	new_item = CopyMediaItem(sel_item, track, 10)
 	reaper.UpdateArrange()
+	item_guid = reaper.BR_GetMediaItemGUID(sel_item)
+	new_item_guid = reaper.BR_GetMediaItemGUID(new_item)
 end
+    

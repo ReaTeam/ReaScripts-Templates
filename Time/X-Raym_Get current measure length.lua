6d979bdc07fb86a9c6e2abb7d7728c7b1e605500
X-Raym <extremraym@yahoo.fr>
2016-05-06 15:20:38 +0200

    Initial release

---
+++ Time/X-Raym_Get current measure length.lua
@@ -0,0 +1,18 @@
+function GetPlayOrEditCursorPos()
+	local play_state = reaper.GetPlayState()
+	local cursor_pos
+	if play_state == 1 then
+		cursor_pos = reaper.GetPlayPosition()
+	else
+		cursor_pos = reaper.GetCursorPosition()
+	end
+	return cursor_pos
+end
+
+function GetCurrentMeasureLength() -- from edit cursor or play position
+	local cursor_pos = GetPlayOrEditCursorPos()
+	local etval, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats(0, cursor_pos)
+	local current_measure = reaper.TimeMap2_beatsToTime(0, retval, measures)
+	local next_beat = reaper.TimeMap2_beatsToTime(0, retval + 1, measures + look_ahead_measures)
+	return next_measure - current_measure
+end

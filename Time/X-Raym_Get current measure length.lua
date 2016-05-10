d80174109b8076f1b9c75b27f11fe570e8c90a12
X-Raym <extremraym@yahoo.fr>
2016-05-10 13:19:12 +0200

    update

--- Time/X-Raym_Get current measure length.lua
+++ Time/X-Raym_Get current measure length.lua
@@ -18,3 +18,13 @@ function GetCurrentMeasureLength() -- from edit cursor or play position
 end
 
 current_measure_length = GetCurrentMeasureLength()
+
+-- http://forum.cockos.com/showpost.php?p=1678100&postcount=12
+-- by benf
+function GetCurrentMeasureLength()
+  local cursor_pos = GetPlayOrEditCursorPos()
+  local retval, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats(0, cursor_pos)
+  local current_measure = reaper.TimeMap2_beatsToTime(0, fullbeats)
+  local next_measure = reaper.TimeMap2_beatsToTime(0, fullbeats + cml)
+  return next_measure - current_measure
+end

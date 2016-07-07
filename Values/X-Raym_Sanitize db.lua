7dece9f809d6d1f82603d22aa1a808797e982f3a
X-Raym <extremraym@yahoo.fr>
2016-07-07 18:18:26 +0200

    Update X-Raym_Sanitize db.lua

--- Values/X-Raym_Sanitize db.lua
+++ Values/X-Raym_Sanitize db.lua
@@ -1,4 +1,4 @@
-function SanitizeDB( val, min, max, default )
+function SanitizeDB( val, min_val, max_val, default )
 
 	-- TYPE
 	if not type( val ) == "number" then return default end
@@ -10,11 +10,17 @@ function SanitizeDB( val, min, max, default )
 	val = math.floor( val + 0.5 )
 
 	-- LIMITS
-	if val < min then val = min end
-	if val > max then val = max end
+	if val < min_val then val = min_val end
+	if val > max_val then val = max_val end
 
 	return val
 
 end
 
-val = SanitizeDB( 150, 0, 100, 72 )
+val = -30
+
+min_val = -144
+max_val = 0
+default_val = -50
+
+val = SanitizeDB( val, min_val, max_val, default_val )

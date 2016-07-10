baf3d0171a91df1dbc3a33b624c457efc15729c6
X-Raym <extremraym@yahoo.fr>
2016-07-10 10:18:14 +0200

    Initial release

---
+++ Various/X-Raym_Check if SWS is installed and download if not.lua
@@ -0,0 +1,19 @@
+function Msg(variable)
+  reaper.ShowConsoleMsg(tostring(variable).."\n")
+end
+
+function Open_URL(url)
+  if not OS then local OS = reaper.GetOS() end
+  if OS=="OSX32" or OS=="OSX64" then
+    os.execute("open ".. url)
+   else
+    os.execute("start ".. url)
+  end
+end
+
+if reaper.BR_SetMediaTrackLayouts then
+  local retval = reaper.ShowMessageBox("SWS extension is required by this script.\nHowever, it doesn't seem to be present for this REAPER installation.\n\nDo you want to download it now ?", "Warning", 1)
+  if retval == 1 then
+    Open_URL("http://www.sws-extension.org/download/pre-release/")
+  end
+end

933f52be85567bc5296a0755358b4731e09e1c48
X-Raym <extremraym@yahoo.fr>
2016-07-11 17:18:24 +0200

    Update X-Raym_Check if SWS is installed and download if not.lua

--- Various/X-Raym_Check if SWS is installed and download if not.lua
+++ Various/X-Raym_Check if SWS is installed and download if not.lua
@@ -8,18 +8,13 @@ function Open_URL(url)
 end
 
 function CheckSWS()
-  local reaperkey = {}
-  for k, v in pairs(reaper) do
-    reaperkey[k] = 0
-  end
-  
-  if reaperkey["BR_SetMediaTracksLayout"] then 
-    return true
-  else
+  if reaper.NamedCommandLookup("_BR_VERSION_CHECK") == 0 then 
     local retval = reaper.ShowMessageBox("SWS extension is required by this script.\nHowever, it doesn't seem to be present for this REAPER installation.\n\nDo you want to download it now ?", "Warning", 1)
     if retval == 1 then
       Open_URL("http://www.sws-extension.org/download/pre-release/")
     end
+  else
+    return true
   end
 end
 

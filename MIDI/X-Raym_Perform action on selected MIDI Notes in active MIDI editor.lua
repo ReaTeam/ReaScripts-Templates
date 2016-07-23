f5b5eff2a276b6b32cb633729309edc4def13cdb
X-Raym <extremraym@yahoo.fr>
2016-07-23 15:05:12 +0200

    Create X-Raym_Perform action on selected MIDI Notes in active MIDI editor.lua

---
+++ MIDI/X-Raym_Perform action on selected MIDI Notes in active MIDI editor.lua
@@ -0,0 +1,54 @@
+--[[
+ * ReaScript Name: Mute selected notes in open MIDI take randomly
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Licence: GPL v3
+ * REAPER: 5.0 pre 15
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2016-07-21)
+	+ Initial Release
+--]]
+
+local reaper = reaper
+
+function Main( take )
+  		
+  		retval, notes, ccs, sysex = reaper.MIDI_CountEvts( take )
+
+  		-- GET SELECTED NOTES (from 0 index)
+  		for k = 0, notes - 1 do
+	  				
+	  		local retval, sel, muted, startppqposOut, endppqposOut, chan, pitch, vel = reaper.MIDI_GetNote( take, k )
+
+	  		if sel then
+
+	  			-- ACTION HERE
+	  			
+	  		end
+	  			
+	  	end
+
+end
+
+-------------------------------
+-- INIT
+-------------------------------
+
+take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+
+if take then
+	
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+  
+  Main( take ) -- Execute your main function
+  
+  reaper.Undo_EndBlock("Mute selected note in open MIDI take randomly", 0) -- End of the undo block. Leave it at the bottom of your main function.
+  
+  reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+end -- ENDIF Take is MIDI

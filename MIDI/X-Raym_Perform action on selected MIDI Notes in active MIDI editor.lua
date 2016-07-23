7ca249a1a37f545b19885ca6a2b18a3a0248b351
X-Raym <extremraym@yahoo.fr>
2016-07-23 15:06:37 +0200

    Update X-Raym_Perform action on selected MIDI Notes in active MIDI editor.lua

--- MIDI/X-Raym_Perform action on selected MIDI Notes in active MIDI editor.lua
+++ MIDI/X-Raym_Perform action on selected MIDI Notes in active MIDI editor.lua
@@ -18,20 +18,20 @@ local reaper = reaper
 
 function Main( take )
   		
-  		retval, notes, ccs, sysex = reaper.MIDI_CountEvts( take )
-
-  		-- GET SELECTED NOTES (from 0 index)
-  		for k = 0, notes - 1 do
-	  				
-	  		local retval, sel, muted, startppqposOut, endppqposOut, chan, pitch, vel = reaper.MIDI_GetNote( take, k )
-
-	  		if sel then
-
-	  			-- ACTION HERE
-	  			
-	  		end
-	  			
-	  	end
+	retval, notes, ccs, sysex = reaper.MIDI_CountEvts( take )
+	
+	-- GET SELECTED NOTES (from 0 index)
+	for k = 0, notes - 1 do
+	  		
+		local retval, sel, muted, startppqposOut, endppqposOut, chan, pitch, vel = reaper.MIDI_GetNote( take, k )
+		
+		if sel then
+		
+		  	-- ACTION HERE
+		  
+		end
+	  	
+	end
 
 end
 
@@ -39,16 +39,16 @@ end
 -- INIT
 -------------------------------
 
-take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+take = reaper.MIDIEditor_GetTake( reaper.MIDIEditor_GetActive() )
 
 if take then
 	
 	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
-  
-  Main( take ) -- Execute your main function
-  
-  reaper.Undo_EndBlock("Mute selected note in open MIDI take randomly", 0) -- End of the undo block. Leave it at the bottom of your main function.
-  
-  reaper.UpdateArrange() -- Update the arrangement (often needed)
+	
+	Main( take ) -- Execute your main function
+	
+	reaper.Undo_EndBlock("Mute selected note in open MIDI take randomly", 0) -- End of the undo block. Leave it at the bottom of your main function.
+	
+	reaper.UpdateArrange() -- Update the arrangement (often needed)
 
 end -- ENDIF Take is MIDI

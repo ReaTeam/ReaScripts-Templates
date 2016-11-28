015c0f722441631d982fbb4702f0aa17201932d7
Raymond Radet <X-Raym@users.noreply.github.com>
2016-11-28 21:22:37 +0100

    Update X-Raym_Get action or project variable state.lua

--- Various/X-Raym_Get action or project variable state.lua
+++ Various/X-Raym_Get action or project variable state.lua
@@ -1,4 +1,7 @@
 -- Action exemple : Record: Set record mode to selected item auto-punch (ID: 40253)
 
+-- Project Value style (need SWS)
 retval, value = reaper.SNM_GetIntConfigVar( 'projrecmode', -666)
+
+-- Action state on/off style
 value = reaper.GetToggleCommandState( 40253 )

ad2fa80d7d5d70a26536124b0c6c9797771df8f9
Raymond Radet <X-Raym@users.noreply.github.com>
2016-12-01 13:15:31 +0100

    Create X-Raym_Joystick API test.lua

---
+++ Various/X-Raym_Joystick API test.lua
@@ -0,0 +1,25 @@
+function Main()
+
+  time = os.clock()
+  
+  joystick_button_mask = reaper.joystick_getbuttonmask( joystick_device )
+  joystick_pov = reaper.joystick_getpov( joystick_device, 0 )
+  
+  joystick_x1_axis = reaper.joystick_getaxis( joystick_device, 0 )
+  joystick_y1_axis = reaper.joystick_getaxis( joystick_device, 1 )
+  joystick_x2_axis = reaper.joystick_getaxis( joystick_device, 2 )
+  joystick_y2_axis = reaper.joystick_getaxis( joystick_device, 3 )
+  
+  
+  reaper.defer( Main )
+  
+end
+
+-- Create Joystick
+index = 0
+joystick_guid, joystick_name = reaper.joystick_enum( index )
+joystick_device = reaper.joystick_create( joystick_guid )
+joystick_buttons, joystick_axes, joystick_povs = reaper.joystick_getinfo( joystick_device )
+
+-- RUN
+Main()

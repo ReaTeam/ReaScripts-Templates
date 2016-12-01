dd2e9e6b08b896d7c9ce37712b977a9439087395
Christian Fillion <cfillion@users.noreply.github.com>
2016-11-30 19:02:32 -0500

    Create cfillion_Clipboard Copy and Paste.lua

---
+++ User-Inputs/cfillion_Clipboard Copy and Paste.lua
@@ -0,0 +1,44 @@
+-- Extracted from cfillion_Interactive ReaScript.lua
+
+--[[ USAGE
+copy("hello world");
+local text = paste(); --> "hello world"
+--]]
+
+local function iswindows()
+  return reaper.GetOS():find('Win') ~= nil
+end
+
+local function ismacos()
+  return reaper.GetOS():find('OSX') ~= nil
+end
+
+local function copy(text)
+  local tool
+
+  if ismacos() then
+    tool = 'pbcopy'
+  elseif iswindows() then
+    tool = 'clip'
+  end
+
+  local proc = assert(io.popen(tool, 'w'))
+  proc:write(text)
+  proc:close()
+end
+
+local function paste()
+  local tool
+
+  if ismacos() then
+    tool = 'pbpaste'
+  elseif iswindows() then
+    tool = 'powershell -windowstyle hidden -Command Get-Clipboard'
+  end
+
+  local proc = assert(io.popen(tool, 'r'))
+  local text = proc:read("*all")
+  proc:close()
+  
+  return text
+end

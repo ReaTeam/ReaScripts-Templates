3e691bba3888dfdff2d1bac86189699c8c04c8ec
Raymond Radet <X-Raym@users.noreply.github.com>
2016-12-08 19:13:18 +0100

    Create X-Raym_Perform action on selected CC events.lua

---
+++ MIDI/X-Raym_Perform action on selected CC events.lua
@@ -0,0 +1,54 @@
+--[[
+ * ReaScript Name: 
+ * Author: 
+ * Author URI: 
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2016-07-21)
+	+ Initial Release
+--]]
+
+local reaper = reaper
+
+function Main( take )
+  		
+	retval, notes, ccs, sysex = reaper.MIDI_CountEvts( take )
+	
+	-- GET SELECTED NOTES (from 0 index)
+	for k = 0, ccs - 1 do
+
+    local retval, sel, muted, startppq, chanmsg, chan, msg2, val = reaper.MIDI_GetCC( take, ccidx )
+		
+		if sel then
+		
+			-- ACTION HERE
+      reaper.MIDI_SetCC( take, k, sel, muted, startppq, chanmsg, chan, msg2, val, false )
+		
+		end
+	  	
+	end
+
+end
+
+-------------------------------
+-- INIT
+-------------------------------
+
+take = reaper.MIDIEditor_GetTake( reaper.MIDIEditor_GetActive() )
+
+if take then
+	
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+	
+	Main( take ) -- Execute your main function
+	
+	reaper.Undo_EndBlock("", 0) -- End of the undo block. Leave it at the bottom of your main function.
+	
+	reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+end -- ENDIF Take is MIDI

dec7080da0f618946736388897cca0926abf8143
Raymond Radet <X-Raym@users.noreply.github.com>
2017-02-23 02:33:46 +0100

    Create X-Raym_Put variables return by a function in an array.lua

---
+++ Lua/X-Raym_Put variables return by a function in an array.lua
@@ -0,0 +1,13 @@
+function table.pack(...) -- For Lua < 5.2  https://www.lua.org/manual/5.2/manual.html#pdf-table.pack
+      return { n = select("#", ...), ... }
+end
+
+function test()
+    return "4", "3", "2", "1"
+end
+
+output = table.pack( test() )
+
+for i, v in ipairs (output) do
+  print(v)
+end

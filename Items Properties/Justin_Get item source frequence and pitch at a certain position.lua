1ca0fc7b12a328d8abbaa79c5436e060bd6c67f9
Raymond Radet <X-Raym@users.noreply.github.com>
2017-03-05 19:37:06 +0100

    Update Justin_Get item source frequence and pitch at a certain position.lua

--- Items Properties/Justin_Get item source frequence and pitch at a certain position.lua
+++ Items Properties/Justin_Get item source frequence and pitch at a certain position.lua
@@ -12,3 +12,28 @@ if rv & (1<<24) and (rv&0xfffff) > 0 then
   spl = buf[nch*ns*2 + 1];
   reaper.MB(string.format("Pitch: %d Hz, tonality %f",spl&0x7fff,(spl>>15)/16384.0),"hi",0);
 end
+
+
+-- X-Raym Mod
+-- same thing as a function
+function GetSourceItemFrequencyAtPosition( take, pos )
+  local source = reaper.GetMediaItemTake_Source(take)
+  local nch = reaper.GetMediaSourceNumChannels(source)
+  local ns = 1
+  local buf = reaper.new_array(ns*3*nch)
+  local rv = reaper.GetMediaItemTake_Peaks(take,1000.0,pos,nch,ns,115,buf)
+  if rv & (1<<24) and (rv&0xfffff) > 0 then
+    local spl = buf[nch*ns*2 + 1]
+    return tonumber(string.format("%d",spl&0x7fff))
+   end 
+end
+
+item = reaper.GetSelectedMediaItem(0,0)
+if item then
+  take = reaper.GetActiveTake(item,0)
+  if take and not reaper.TakeisMIDI(take) then
+    pos = reaper.GetCursorPosition()
+    freq = GetSourceItemFrequencyAtPosition( take, pos )
+  end
+ end 
+ 

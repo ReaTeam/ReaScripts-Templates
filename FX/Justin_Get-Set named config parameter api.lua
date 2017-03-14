1b0d580fa333e98db1e659182f9d157f2e4173ef
Raymond Radet <X-Raym@users.noreply.github.com>
2017-03-14 18:19:46 +0100

    Create Justin_Get-Set named config parameter api.lua

---
+++ FX/Justin_Get-Set named config parameter api.lua
@@ -0,0 +1,17 @@
+-- http://forum.cockos.com/showpost.php?p=1817782&postcount=5
+
+ok, fn = reaper.TrackFX_GetNamedConfigParm(track,fx, "FILE0") -- get first sample filename
+ok, fn2 = reaper.TrackFX_GetNamedConfigParm(track,fx, "FILE1") -- get second sample filename
+
+
+ok = reaper.TrackFX_SetNamedConfigParm(track,fx, "FILE0", fn) -- set first sample filename
+ok = reaper.TrackFX_SetNamedConfigParm(track,fx, "FILE1", fn2) -- set second sample filename -- note that FILE0 must exist before you can set FILE1, etc.
+
+ok = reaper.TrackFX_SetNamedConfigParm(track,fx, "-FILE0", "") -- remove first sample (second sample becomes first sample)
+
+ok = reaper.TrackFX_SetNamedConfigParm(track,fx, "+FILE0", fn) -- insert sample (old FILE0 becomes FILE1)
+
+ok = reaper.TrackFX_SetNamedConfigParm(track,fx, "-FILE*", "") -- remove all samples
+
+-- Finally, when the script is finished updating the samples, it should call (this will trigger actually loading the samples from disk, adding an undo point, etc):
+reaper.TrackFX_SetNamedConfigParm(track,fx, "DONE","")

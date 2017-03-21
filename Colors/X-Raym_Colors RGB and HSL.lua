c3388faba9c05bb5b2a9190ce47b9e22b23613cd
Raymond Radet <X-Raym@users.noreply.github.com>
2017-03-21 22:36:23 +0100

    Create X-Raym_Colors RGB and HSL.lua

---
+++ Colors/X-Raym_Colors RGB and HSL.lua
@@ -0,0 +1,86 @@
+-- COLORS -------------------------------------------------------------
+
+-- From https://github.com/EmmanuelOga/columns/blob/master/utils/color.lua
+
+function hue2rgb(p, q, t)
+  if t < 0   then t = t + 1 end
+  if t > 1   then t = t - 1 end
+  if t < 1/6 then return p + (q - p) * 6 * t end
+  if t < 1/2 then return q end
+  if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+  return p
+end
+
+--[[
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+]]
+function rgbToHsl(r, g, b, a)
+  r, g, b = r / 255, g / 255, b / 255
+
+  local max, min = math.max(r, g, b), math.min(r, g, b)
+  local h, s, l
+
+  l = (max + min) / 2
+
+  if max == min then
+    h, s = 0, 0 -- achromatic
+  else
+    local d = max - min
+    local s
+    if l > 0.5 then s = d / (2 - max - min) else s = d / (max + min) end
+    if max == r then
+      h = (g - b) / d
+      if g < b then h = h + 6 end
+    elseif max == g then h = (b - r) / d + 2
+    elseif max == b then h = (r - g) / d + 4
+    end
+    h = h / 6
+  end
+
+  return h, s, l, a or 255
+end
+
+--[[
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+]]
+function hslToRgb(h, s, l, a)
+  local r, g, b
+
+  if s == 0 then
+    r, g, b = l, l, l -- achromatic
+  else
+
+    local q
+    if l < 0.5 then q = l * (1 + s) else q = l + s - l * s end
+    local p = 2 * l - q
+
+    r = hue2rgb(p, q, h + 1/3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1/3)
+  end
+  r = math.floor((r * 255) + 0.5)
+  g = math.floor((g * 255) + 0.5)
+  b = math.floor((b * 255) + 0.5)
+  return r, g, b, a * 255
+end
+
+function RGB2INT ( R, G, B )
+	local color = (R + 256 * G + 65536 * B)|16777216
+	return color
+end

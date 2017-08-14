55553f2c013ac8ba484b57a5d21bf0a3729b56c0
Raymond Radet <X-Raym@users.noreply.github.com>
2017-08-14 16:04:24 +0200

    Update X-Raym_List all globals and reaper variables and functions in the console.lua

--- Various/X-Raym_List all globals and reaper variables and functions in the console.lua
+++ Various/X-Raym_List all globals and reaper variables and functions in the console.lua
@@ -12,3 +12,14 @@ Msg("\nREAPER Functions")
 for k, v in pairs(reaper) do
   --reaper.ShowConsoleMsg( k .. " = " .. tostring(v) .. "\n")
 end
+
+-- List Maths Functions
+reaper.ClearConsole()
+maths = {}
+for k,v in pairs( math ) do
+	table.insert(maths, k)
+end
+table.sort(maths)
+for k,v in pairs( maths ) do
+   reaper.ShowConsoleMsg( tostring(v) .. "\n")
+end

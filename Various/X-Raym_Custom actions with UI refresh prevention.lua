7ca4f77dc99bc441632b2e96b1afab4d12363dbd
Raymond Radet <X-Raym@users.noreply.github.com>
2017-09-09 12:15:58 +0200

    Update X-Raym_Custom actions with UI refresh prevention.lua

--- Various/X-Raym_Custom actions with UI refresh prevention.lua
+++ Various/X-Raym_Custom actions with UI refresh prevention.lua
@@ -1,6 +1,7 @@
 reaper.PreventUIRefresh(1) -- Prevent UI Refresh
 
-reaper.Main_OnCommand(4000, 0)-- Call to native Actions. Command ID can be found in the action list.
-reaper.Main_OnCommand( reaper.NamedCommandLookup("_SWS_SELTRKWITEM"),  0) -- Call to SWS Actions.
+-- Duplicates and modify the order of the two following lines at will.
+reaper.Main_OnCommand( 4000, 0 )-- Call to native Actions. Replace 4000 by your choosen Command ID. Command ID can be found in the action list.
+reaper.Main_OnCommand( reaper.NamedCommandLookup( "_SWS_SELTRKWITEM"),  0 ) -- Call to SWS Actions. Replace "_SWS_SELTRKWITEM" by Command ID of your SWS action.
 
 reaper.PreventUIRefresh(-1) -- Restore UI Refresh

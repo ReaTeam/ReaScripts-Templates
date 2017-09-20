97572ef621347c00490ffe9ff56f736a349ef903
Raymond Radet <X-Raym@users.noreply.github.com>
2017-09-20 18:25:16 +0200

    Update X-Raym_Multisplit items.lua

--- Items Creations/X-Raym_Multisplit items.lua
+++ Items Creations/X-Raym_Multisplit items.lua
@@ -2,25 +2,34 @@
 -- prevent bad value insertion
 -- sort table
 function MultiSplitMediaItem(item, times)
-	
-	-- create array then reserve some space in array
-	local items = {}
-	
-	-- add 'item' to 'items' array
-	table.insert(items, item)
-	
-	-- for each time in times array do...
-	for i, time in ipairs(times) do
-		
-		-- store item so we can split it next time around
-		item = SplitMediaItem(item, time)
-		
-		-- add resulting item to array
-		table.insert(items, item)
 
-	end
+  local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+  local item_end = reaper.GetMediaItemInfo_Value(item, "D_LENGTH") + item_pos
+  
+  -- create array then reserve some space in array
+  local items = {}
+  
+  -- add 'item' to 'items' array
+  table.insert(items, item)
+  
+  -- for each time in times array do...
+  for i, time in ipairs(times) do
+  
+    if time > item_end then break end
+    
+    if time > item_pos and time < item_end then
+  
+      -- store item so we can split it next time around
+      item = reaper.SplitMediaItem(item, time)
+      
+      -- add resulting item to array
+      table.insert(items, item)
+      
+    end
 
-	-- return 'items' array
-	return items
+  end
+
+  -- return 'items' array
+  return items
 
 end

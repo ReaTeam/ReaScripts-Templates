382ae5198bd62cdd5ecfbd02dc1b8820dc6344e1
Raymond Radet <X-Raym@users.noreply.github.com>
2017-09-24 18:58:05 +0200

    Create X-Raym_Basic tkinter python.py

---
+++ GUI/X-Raym_Basic tkinter python.py
@@ -0,0 +1,16 @@
+from reaper_python import *
+# from sws_python import *
+
+import sys
+
+sys.argv=["Main"]
+
+import tkinter
+from tkinter import *
+
+root = Tk()
+
+label = Label(root, text="Hello World")
+label.pack()
+
+root.mainloop()

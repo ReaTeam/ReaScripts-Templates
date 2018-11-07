1a86f1b800db3d645218633598825cb80e2d57fa
nofish <nofishonfriday@googlemail.com>
2018-11-07 01:09:10 +0100

    update NF_Get SWS markers and regions (since ReaScript functions have changed)

--- Markers and Regions/NF_Get SWS markers and regions notes.lua
+++ Markers and Regions/NF_Get SWS markers and regions notes.lua
@@ -6,36 +6,32 @@ function msg(m)
 end
 
 -- set subs
-fastStringIn = reaper.SNM_CreateFastString("")
 count_markers_and_regions, count_regions = reaper.CountProjectMarkers(0)
 for i = 0, count_markers_and_regions - 1 do
   local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut =  reaper.EnumProjectMarkers( i )
   
   if isrgnOut == true then 
-    reaper.SNM_SetFastString(fastStringIn, "new Subtitle text for region " .. markrgnindexnumberOut)
+    mkrRgnSubIn = "new Subtitle text for region " .. markrgnindexnumberOut
   else
-    reaper.SNM_SetFastString(fastStringIn, "new Subtitle text for marker " .. markrgnindexnumberOut)
+    mkrRgnSubIn = "new Subtitle text for marker " .. markrgnindexnumberOut
   end
   
-  regionSubIsSet = reaper.NF_SetSWSMarkerRegionSub(fastStringIn, i)
+  regionSubIsSet = reaper.NF_SetSWSMarkerRegionSub(mkrRgnSubIn, i)
   i = i + 1
 end
-reaper.SNM_DeleteFastString(fastStringIn)
 reaper.NF_UpdateSWSMarkerRegionSubWindow() -- optional
 
 -- get subs
-fastStringOut = reaper.SNM_CreateFastString("")
 count_markers_and_regions, count_regions = reaper.CountProjectMarkers(0)
 for i = 0, count_markers_and_regions - 1 do
   local retval, isrgnOut, posOut, rgnendOut, nameOut, markrgnindexnumberOut =  reaper.EnumProjectMarkers( i )
   markerIdx, regionIdx = reaper.GetLastMarkerAndCurRegion( 0, posOut )
-  regionSub = reaper.NF_GetSWSMarkerRegionSub(fastStringOut, i)
+  local mkrRgnSubOut = reaper.NF_GetSWSMarkerRegionSub(i)
   msg("i = " .. i)
   msg("retval = " .. retval)
   msg("is_region = " .. tostring(isrgnOut))
   msg("markrgnindexnumberOut = " .. markrgnindexnumberOut)
-  msg("NOTES = " .. regionSub)
+  msg("NOTES = " .. mkrRgnSubOut)
   msg("")
   i = i + 1
 end
-reaper.SNM_DeleteFastString(fastStringOut)

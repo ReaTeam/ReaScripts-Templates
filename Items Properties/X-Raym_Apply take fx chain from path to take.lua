36a511297baef4f4b18fd4c1f15c198e0d422260
Raymond Radet <X-Raym@users.noreply.github.com>
2020-02-23 20:12:06 +0100

    Update X-Raym_Apply take fx chain from path to take.lua

--- Items Properties/X-Raym_Apply take fx chain from path to take.lua
+++ Items Properties/X-Raym_Apply take fx chain from path to take.lua
@@ -5,18 +5,25 @@ local retval, ItemStateChunk = reaper.GetItemStateChunk( item, '', false )
 local take_id = reaper.GetMediaItemInfo_Value( item, "I_CURTAKE" )
 
 local item_body, tk_chunks = ExtractTakeChunks(item)
-ItemFXStateChunk = tk_chunks[take_id+1]:match("<TAKEFX(.+)>$") or "" -- get current take FX chain
 
--- Get FX chain content from file
+takeFXStateChunk = tk_chunks[take_id+1]:match("<TAKEFX(.+)>") or ""
+
 file = io.open(fx_chain_path, "r")
 FXStateChunk = file:read("*a"):sub(0,-4)
-FXStateChunk = FXStateChunk:gsub("REQUIRED_CHANNELS %d+\n", "") -- this field is only for tracks so we delete it
+override_take_chan = tonumber(FXStateChunk:match("REQUIRED_CHANNELS (%d+)")) -- this would be only needed for track
+FXStateChunk = FXStateChunk:gsub("REQUIRED_CHANNELS %d+\n", "")
 FXStateChunk = "  " .. FXStateChunk:gsub("\n", "\n  ") -- add indentation
 file:close()
 
-FXStateChunk = "<TAKEFX" .. ItemFXStateChunk .. FXStateChunk .. "\n>"
+current_take_chan = tonumber(tk_chunks[take_id+1]:match("TAKEFX_NCH (%d+)"))
+if current_take_chan and not override_take_chan then override_take_chan = current_take_chan end
+if current_take_chan and override_take_chan and current_take_chan > override_take_chan then override_take_chan = current_take_chan end
 
--- Add FX chain file content to the existing Take Fx chain
+if not override_take_chan then
+  FXStateChunk = "<TAKEFX" .. takeFXStateChunk .. FXStateChunk .. "\n>"
+else
+  FXStateChunk = "<TAKEFX" .. takeFXStateChunk .. FXStateChunk .. "\n>\nTAKEFX_NCH " .. override_take_chan
+end
+
 tk_chunks[take_id+1] = tk_chunks[take_id+1] .. "\n" .. FXStateChunk
-
 ApplyTakeChunks(item,item_body, tk_chunks)

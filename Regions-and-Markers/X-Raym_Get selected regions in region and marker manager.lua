d5507b993f8b82ae481905bc000829e264947067
Raymond Radet <X-Raym@users.noreply.github.com>
2020-08-04 19:03:28 +0200

    Create X-Raym_Get selected regions in region and marker manager.lua

---
+++ Regions-and-Markers/X-Raym_Get selected regions in region and marker manager.lua
@@ -0,0 +1,36 @@
+function Msg(str)
+  reaper.ShowConsoleMsg(tostring(str) .. "\n") -- console
+end
+
+function GetRegionManager()
+  local title = reaper.JS_Localize("Region/Marker Manager", "common")
+  local arr = reaper.new_array({}, 1024)
+  reaper.JS_Window_ArrayFind(title, true, arr)
+  local adr = arr.table()
+  for j = 1, #adr do
+    local hwnd = reaper.JS_Window_HandleFromAddress(adr[j])
+    -- verify window by checking if it also has a specific child.
+    if reaper.JS_Window_FindChildByID(hwnd, 1056) then -- 1045:ID of clear button
+      return hwnd
+    end 
+  end
+end
+
+-- INIT
+reaper.ClearConsole()
+
+local hWnd = GetRegionManager()
+if hWnd == nil then return end  
+
+local container = reaper.JS_Window_FindChildByID(hWnd, 1071)
+
+sel_count, sel_indexes = reaper.JS_ListView_ListAllSelItems(container)
+if sel_count == 0 then return end 
+
+names = {}
+i = 0
+for index in string.gmatch(sel_indexes, '[^,]+') do 
+  i = i+1
+  names[i] = reaper.JS_ListView_GetItemText(container, tonumber(index), 1)
+  Msg(names[i])
+end

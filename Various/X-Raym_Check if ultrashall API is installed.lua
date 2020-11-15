3034550f33765d9910165bde12ce09c1b87b127a
Raymond Radet <X-Raym@users.noreply.github.com>
2020-11-16 00:36:47 +0100

    Update X-Raym_Check if ultrashall API is installed.lua

--- Various/X-Raym_Check if ultrashall API is installed.lua
+++ Various/X-Raym_Check if ultrashall API is installed.lua
@@ -1,7 +1,9 @@
--- DEPENDENCY
-dofile(reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua")
+ultraschall_path = reaper.GetResourcePath().."/UserPlugins/ultraschall_api.lua"
+if reaper.file_exists( ultraschall_path ) then
+  dofile( ultraschall_path )
+end
 
-if not ultraschall and not ultraschall.InsertMediaItem_MediaItem then
-  reaper.MB("Please install Ultrashall API, available via Reapack. Check online doc of the script for more infos.", "Error", 0)
+if not ultraschall or not ultraschall.InsertMediaItem_MediaItem then -- If ultraschall loading failed of if it doesn't have the functions you want to use
+  reaper.MB("Please install Ultrashall API, available via Reapack. Check online doc of the script for more infos.\nhttps://github.com/Ultraschall/ultraschall-lua-api-for-reaper", "Error", 0)
   return
 end

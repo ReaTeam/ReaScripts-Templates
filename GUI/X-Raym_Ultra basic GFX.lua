d2efdb5baddbdfef5983b6fa6dbf3c3fb42c3d76
Raymond Radet <X-Raym@users.noreply.github.com>
2021-02-15 20:22:53 +0100

    Update X-Raym_Ultra basic GFX.lua

--- GUI/X-Raym_Ultra basic GFX.lua
+++ GUI/X-Raym_Ultra basic GFX.lua
@@ -8,15 +8,13 @@ function init(window_w, window_h, window_x, window_y, docked)
 	gfx.init("GFX" , window_w, window_h, docked, window_x, window_y)  -- name,w,h,dockstate,xpos,ypos
 end
 
-function Update()
-	gfx.update()
-	if gfx.getchar() ~= 27 then reaper.defer(run) else gfx.quit() end
-end
-
 -- DRAW IN GFX WINDOW
 function run()
-
-	Update()
+	char = gfx.getchar()
+	
+	
+	gfx.update()
+	if char ~= 27 or char < 0 then reaper.defer(run) else gfx.quit() end
 
 end -- END DEFER
 

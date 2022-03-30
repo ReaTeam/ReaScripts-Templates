6d69649050d47239b42f5c72cce97706ae45d240
Raymond Radet <X-Raym@users.noreply.github.com>
2022-03-30 15:23:10 +0200

    Update Birdbird_GFX line bold.lua

--- GUI/Birdbird_GFX line bold.lua
+++ GUI/Birdbird_GFX line bold.lua
@@ -8,3 +8,17 @@ function gfx_line_bold(x1, y1, x2, y2, w)
   local p3x, p3y, p4x, p4y = x2 - xn*w, y2 - yn*w, x2 + xn*w, y2 + yn*w
   gfx.triangle(p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y)
 end
+
+-- using lines (less efficient but anti aliased)
+function gfx_line_bold2(x1, y1, x2, y2, w)
+  local xn, yn = (x2 - x1)*-1, y2 - y1
+  local l = math.sqrt(xn * xn + yn * yn)
+  xn, yn = xn/l, yn/l
+  local t = xn; xn = yn; yn = t; 
+  local i, fill = 0, 4;
+  while i <= w do
+    gfx.line(x1 + xn*i, y1 + yn*i, x2 + xn*i, y2 + yn*i)
+    gfx.line(x1 - xn*i, y1 - yn*i, x2 - xn*i, y2 - yn*i)
+    i = i + 1/fill
+  end
+end

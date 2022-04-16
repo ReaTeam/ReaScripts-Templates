fb1e7bd19f7d7fce78add011e82b50f8498e0004
Raymond Radet <X-Raym@users.noreply.github.com>
2022-04-16 15:54:58 +0200

    Create X-Raym_String split.lua

---
+++ Strings/X-Raym_String split.lua
@@ -0,0 +1,7 @@
+-- Split CSV string
+function string:split(sep)
+  local sep, fields = sep or ":", {}
+  local pattern = string.format("([^%s]+)", sep)
+  self:gsub(pattern, function(c) fields[#fields+1] = c end)
+  return fields
+end

7df55d46c919ed82db50f726a811deeb3f657ed7
Raymond Radet <X-Raym@users.noreply.github.com>
2022-08-01 13:16:32 +0200

    Update cfillion_check sub keys in array.lua

--- Lua/cfillion_check sub keys in array.lua
+++ Lua/cfillion_check sub keys in array.lua
@@ -7,6 +7,17 @@ function dig(array, ...)
   return true
 end
 
+-- OR, if you want the value directly
+
+function dig(array, ...)
+  for i = 1, select('#', ...) do
+    if type(array) ~= 'table' then return false end
+    array = array[select(i, ...)]
+    if array == nil then return false end
+  end
+  return array
+end
+
 t = { animal = { tetrapod = { dog = true } } }
 print (dig( t, "animal", "tetrapod", "dog" ))
 

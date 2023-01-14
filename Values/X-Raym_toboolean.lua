a3423fa7f5d8f4eb764a63970f71f976aed10b30
Raymond Radet <X-Raym@users.noreply.github.com>
2023-01-14 14:43:07 +0100

    Update X-Raym_toboolean.lua

--- Values/X-Raym_toboolean.lua
+++ Values/X-Raym_toboolean.lua
@@ -1,8 +1,8 @@
 -- Lua: Value to Boolean function
 -- Return false unless tonumber(val) > 0, "true" or "y"
 function toboolean( val )
-  if tonumber(val) and tonumber(val) > 0 then val = "1" end
-  local t = { ["true"] = true, ["y"] = true, ["1"] = true }
+  local nval = tonumber(val)
+  if nval then return nval > 0 end
+  local t = { [true] = true, ["true"] = true, ["y"] = true, ["1"] = true }
   return t[val] or false
 end
-
